52095af90db61d3474d5d94e574b4903fa186878
Sebastian <sebastian@excitedigitalmedia.com>
2013-09-05 09:24:12 +1000

    moving to use bootstrap and modify the content of index to support the semantic

---
+++ lang/en/index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Bicitours - tours en bicicleta Con-Con - Reñaca - Viña del Mar - Valparaíso</title>
+
+        <meta name="description" content="Simplifying Online Search">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/style.css"/>
+        <!--[if lt IE 9]><link rel="stylesheet" type="text/css" href="/css/ie.css"/><!--<![endif]-->
+        <!--[if lt IE 9]> <script src="/js/respond.min.js"></script><!--<![endif]-->
+        <!--[if lt IE 9]><script src="/js/modernizr.custom.js"></script><!--<![endif]-->
+
+
+    </head>
+    <body>
+        <header class="row">
+            <div class='container'>
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="/img/weblogo.png" class="img-responsive" alt="Logo Bicitour" />
+                        </div>
+                        <div class="col-md-6">
+                            <img src="/img/bannertop.png" class="img-responsive" alt="Enjoy tours in Valparaiso Region"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <header class='navbar'>
+                        <div class="navbar-header">
+                            <button class="navbar-toggle btn" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a href="../" class="navbar-brand">Bicitours</a>
+                        </div>
+                        <nav class='collapse navbar-collapse bs-navbar-collapse'>
+                            <ul class="nav navbar-nav">
+                                <li>
+                                    <a href="#">Bicitours</a>
+                                </li>
+                                <li>
+                                    <a href="#">Tours</a>
+                                </li>
+                                <li>
+                                    <a href="#">Rent</a>
+                                </li>
+                                <li class="active">
+                                    <a href="#">Bike sells</a>
+                                </li>
+                                <li>
+                                    <a href="#">Reservation</a>
+                                </li>
+                                <li class="disabled">
+                                    <a href="#">Contact us</a>
+                                </li>
+
+                            </ul>
+                        </nav>
+                    </header>
+                    <div class="carousel slide" id="carousel-464101">
+                        <ol class="carousel-indicators">
+                            <li class="active" data-slide-to="0" data-target="#carousel-464101">
+                            </li>
+                            <li data-slide-to="1" data-target="#carousel-464101">
+                            </li>
+                            <li data-slide-to="2" data-target="#carousel-464101">
+                            </li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img alt="" src="/img/portada1.jpg" />
+                                <div class="carousel-caption">
+                                    <h4>
+                                        First Thumbnail label
+                                    </h4>
+                                    <p>
+                                        Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img alt="" src="/img/portada2.jpg" />
+                                <div class="carousel-caption">
+                                    <h4>
+                                        Second Thumbnail label
+                                    </h4>
+                                    <p>
+                                        Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img alt="" src="/img/portada3.jpg" />
+                                <div class="carousel-caption">
+                                    <h4>
+                                        Third Thumbnail label
+                                    </h4>
+                                    <p>
+                                        Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                                    </p>
+                                </div>
+                            </div>
+                        </div> <a data-slide="prev" href="#carousel-464101" class="left carousel-control">‹</a> <a data-slide="next" href="#carousel-464101" class="right carousel-control">›</a>
+                    </div>
+                    <h3>
+                        h3. Lorem ipsum dolor sit amet.
+                    </h3>
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-3 text-center">
+                            <img src="/img/bike1.png" class="img-thumbnail img-responsive ">
+                            <h6>Mountain Bike</h6>
+                        </div>
+                        <div class="col-xs-6 col-sm-3 text-center">
+                            <img src="/img/casco.png" class="img-thumbnail img-responsive">
+                            <h6>Helmet</h6>
+                        </div>
+                        <div class="col-xs-6 col-sm-3 text-center">
+                            <img src="/img/botella.png" class="img-thumbnail img-responsive">
+                            <h6>Bottled water</h6>
+                        </div>
+                        <div class="col-xs-6 col-sm-3 text-center">
+                            <img src="/img/polera.png" class="img-thumbnail img-responsive">
+                            <h6>Bilingual Guide</h6>
+                        </div>
+                    </div>
+                    <article class="row">
+                        <div class="col-md-6">
+                            <p>
+                                Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+                            <p>
+                                Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+                            </p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </section>
+        <footer class="row">
+            <div class='container'>
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <address> 
+                                <strong>Twitter, Inc.</strong><br /> 
+                                795 Folsom Ave, Suite 600<br /> 
+                                San Francisco, CA 94107<br /> 
+                                <abbr title="Phone">P:</abbr> (123) 456-7890
+                            </address>
+                        </div>
+                        <div class="col-md-6">
+                            <ul class="inline">
+                                <li>
+                                    Lorem ipsum dolor sit amet
+                                </li>
+                                <li>
+                                    Consectetur adipiscing elit
+                                </li>
+                                <li>
+                                    Integer molestie lorem at massa
+                                </li>
+                                <li>
+                                    Facilisis in pretium nisl aliquet
+                                </li>
+                                <li>
+                                    Nulla volutpat aliquam velit
+                                </li>
+                                <li>
+                                    Faucibus porta lacus fringilla vel
+                                </li>
+                                <li>
+                                    Aenean sit amet erat nunc
+                                </li>
+                                <li>
+                                    Eget porttitor lorem
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
+<script src="/js/jquery.min.js"></script>
+<script src='/js/bootstrap.min.js'></script>
